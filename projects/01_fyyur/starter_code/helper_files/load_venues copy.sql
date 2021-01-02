d61ceb0e83e518f6f0cb05699dc74afed6469173
projectramo <projectramo@gmail.com>
2021-01-02 16:25:24 -0600

    more fields and load_venues almost there

--- projects/01_fyyur/starter_code/helper_files/load_venues copy.sql
+++ projects/01_fyyur/starter_code/helper_files/load_venues copy.sql
@@ -12,7 +12,7 @@ with venue_json (doc) as (
     "facebook_link": "https://www.facebook.com/TheMusicalHop",
     "seeking_talent": "True",
     "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
   },
   {
     "id": 2,
@@ -25,7 +25,7 @@ with venue_json (doc) as (
     "website": "https://www.theduelingpianos.com",
     "facebook_link": "https://www.facebook.com/theduelingpianos",
     "seeking_talent": "False",
-    "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
   },
   {
     "id": 3,
@@ -38,13 +38,14 @@ with venue_json (doc) as (
     "website": "https://www.parksquarelivemusicandcoffee.com",
     "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
     "seeking_talent": "False",
-    "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80"
+    "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
   }]'::json)
 )
 insert into "Venue" (id, name, genres, address, city, state, phone, website, facebook_link, seeking_talent, seeking_description, image_link)
 select p.*
 from venue_json l
-  cross join lateral json_populate_recordset(null::"Venue", doc) as p
+  cross join lateral json_populate_recordset(null::venue, doc) as p
 on conflict (id) do update 
-  set name = excluded.name;
+  set name = excluded.name, 
+      comment = excluded.comment;
 
